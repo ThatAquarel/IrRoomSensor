321edbdbbb8009096152675e30e2dc5c2c1a0c74
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2021-04-24 09:35:30 -0400

    Rename

--- tests/denoise.py
+++ tests/denoise.py
@@ -7,19 +7,16 @@ image_raw = np.where(image_raw < 0, mean, image_raw)
 
 image_mod = np.copy(image_raw)
 
-mean = np.mean(image_mod)
-image_mod -= mean
-a_max = np.amax(image_mod)
-image_mod /= a_max
-
 a_min = np.amin(image_mod)
-image_mod -= a_min
 a_max = np.amax(image_mod)
-image_mod /= a_max
 
-image_mod = np.power(image_mod, 10)
+image_mod = (image_mod - a_min) / (a_max - a_min)
 
-fig, ax = plt.subplots(1, 2)
+image_pow = np.power(image_mod, 10)
+image_exp = np.power(10, image_mod)
+
+fig, ax = plt.subplots(1, 3)
 ax[0].imshow(image_raw)
-ax[1].imshow(image_mod)
+ax[1].imshow(image_pow)
+ax[2].imshow(image_exp)
 plt.show()
